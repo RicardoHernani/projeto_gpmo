40df6e298ecdee7cec028126a0afc65b6ab515b7
RicardoHernani <ricardohernani@yahoo.com.br>
2021-01-23 16:51:36 -0300

    Criando operação de instanciação

--- src/main/java/com/ricardohernani/projeto_gpmo/ProjetoGpmoApplication.java
+++ src/main/java/com/ricardohernani/projeto_gpmo/ProjetoGpmoApplication.java
@@ -1,12 +1,34 @@
 package com.ricardohernani.projeto_gpmo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoGpmoApplication {
+import com.ricardohernani.projeto_gpmo.domain.Paciente;
+import com.ricardohernani.projeto_gpmo.repositories.PacienteRepository;
 
+@SpringBootApplication
+public class ProjetoGpmoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PacienteRepository pacienteRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoGpmoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Paciente pcte1 = new Paciente(null, 12345678);
+		Paciente pcte2 = new Paciente(null, 11223344);
+		
+		pacienteRepository.saveAll(Arrays.asList(pcte1, pcte2));
+		
+	}
+	
+	
 }
