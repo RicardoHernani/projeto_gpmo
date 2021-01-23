73ea760a6fc09a300564fba2b26f5a41e0e9fae2
RicardoHernani <ricardohernani@yahoo.com.br>
2021-01-22 21:45:04 -0300

    Projeto criado

---
+++ src/main/java/com/ricardohernani/projeto_gpmo/ProjetoGpmoApplication.java
@@ -0,0 +1,13 @@
+package com.ricardohernani.projeto_gpmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGpmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGpmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardohernani/projeto_gpmo/ProjetoGpmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardohernani.projeto_gpmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGpmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
